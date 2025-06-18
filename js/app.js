806329d53b9fec8d2d561c74a0670fd9e4d1f9bc
Jana Šlancarová <jana.slancar@gmail.com>
2025-06-18 22:59:55 +0200

    uprava ham menu

--- js/app.js
+++ js/app.js
@@ -2,7 +2,23 @@ const hamMenu = document.querySelector(".ham-menu");
 
 const Menu = document.querySelector(".header-desktop .menu");
 
+// hamMenu.addEventListener("click", () => {
+//   hamMenu.classList.toggle("active");
+//   Menu.classList.toggle("active");
+// });
+
+
 hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   Menu.classList.toggle("active");
 });
+
+document.addEventListener("click", clickOutside);
+function clickOutside(e) { 
+   const inside = Menu.contains(e.target) || hamMenu.contains(e.target);
+   if (!inside) {
+      hamMenu.classList.remove("active");
+      Menu.classList.remove("active");
+   }
+}
+
